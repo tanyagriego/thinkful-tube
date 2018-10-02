9c2e6eac86b455a1dfdbe8d5e455c6fb1259f664
tanyagriego <tanya.griego@gmail.com>
2018-10-02 08:44:06 -0700

    Outlined functions for displaying data to user

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const YouTube_Search_URL = "https://www.googleapis.com/youtube/v3/search";
 
+
+//Get data from API
 function getDataFromApi (searchTerm, callback){
     const setting = {
         url: YouTube_Search_URL,
@@ -16,3 +18,15 @@ function getDataFromApi (searchTerm, callback){
     $.ajax(settings);
 }
 
+//Does something with the data (callback)
+function displayYouTubeSearchData (data) {
+    //loop through array items in object data?
+    const results = data.
+    //render results to another function which creates html
+    
+    //put the html into the search results div
+    $('.search-results').html();
+}
+
+
+//Appends data to the DOM
